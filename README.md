f82f50a0ab7966639ba74d99da04f75337a8cc49
Drew Anderson <drw.a@outlook.com>
2017-07-18 23:38:45 -0400

    es6 options and modified readme

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # doublecookie
-Basic CSRF prevention middleware using double submit cookie  
+Basic CSRF prevention middleware using a double submit cookie
+
+This was designed to be drop-in middleware.  app.use(doublecookie()) accepting all the defaults, then pass locals.postCheck into your template as a hidden form field.
+
 [See csurf first](https://github.com/expressjs/csurf)  
-[owasp csrf](https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)_Prevention_Cheat_Sheet)  
+[owasp csrf](https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)_Prevention_Cheat_Sheet)
+[Liran Tal video - Node JS: Security Breaking the Loop](https://www.youtube.com/watch?v=DX8FSC_7wRI)
--- index.js
+++ index.js
@@ -7,15 +7,22 @@ const random = require('randomatic')
  * for use in a hidden form input.
  * 2. Compares the cookie and hash on each post-like request, Err if no match
  *
- * @param {object} cfg Configuration object
- * @param {bool} cfg.always Set True to refresh cookie on each request
+ * @param {object} options Configuration object
+ * @param {bool} options.always Default false.  Set true to refresh cookie on each request, but this'll break back button.
+ * @param {bool} options.secure Default true.  You should always use https in production.  False ok for development
+ * @param {bool} options.httpOnly Default true. Flags the cookie to be accessible only by the web server
+ * @param {bool} options.signed Default true.  Requires a signature from cookie-parser.  Load that middleware first.
  * @api public
  */
 
-const doubleCookie = function (cfg) {
+const doubleCookie = function (options) {
 
-    cfg = cfg || {}
-    cfg.always = cfg.always || false
+    let {
+        always = false,
+        secure = true,
+        httpOnly = true,
+        signed = true
+    } = options
 
     const middleware = function (req, res, next) {
 
@@ -29,15 +36,16 @@ const doubleCookie = function (cfg) {
         if (postMethods.includes(req.method)) {
             // If there's no cookie or the cookie doesn't match its hash
             if ((!req.signedCookies.dblCookie) || hash(req.signedCookies.dblCookie) !== req.body.postCheck) {
+                console.log("double", req.signedCookies.dblCookie)
                 return (next(new Error('DoubleCookie CSRF')))
             } 
         }
 
         const reload = function () {
-            // If either there's no dblCookie or cfg says to always reload, reload
-            if (!req.signedCookies.dblCookie || cfg.always) {
+            // If either there's no dblCookie yet or options says to always refresh, reload
+            if (!req.signedCookies.dblCookie || always) {
                 var newCookie = random('*',64)
-                res.cookie('dblCookie', newCookie, { signed: true, httpOnly: true })
+                res.cookie('dblCookie', newCookie, { signed: signed, httpOnly: httpOnly, secure: secure })
             }
             const dblCookie = newCookie || req.signedCookies.dblCookie
             res.locals.postCheck = hash(dblCookie)
