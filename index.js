80d98a3eab3747a4b92f45de979cad25d9b7b1c1
Drew Anderson <drw.a@outlook.com>
2017-09-17 03:19:03 -0400

    removed randomatic dependency and replaed with crypto randomBytes.  Added param default {} for options.  Added option to override post method.

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 const crypto = require('crypto')
-const random = require('randomatic')
 
 /**
  * Simple middleware for double request cookie CSRF defense: 
  * 1. Sets a session cookie and puts a hash of that cookie in res.locals.postCheck 
  * for use in a hidden form input.
- * 2. Compares the cookie and hash on each post-like request, Err if no match
+ * 2. Compares the cookie and hash on each request, Err if no match
  *
  * @param {object} options Configuration object
  * @param {bool} options.always Default false.  Set true to refresh cookie on each request, but this'll break back button.
  * @param {bool} options.secure Default true.  You should always use https in production.  False ok for development
  * @param {bool} options.httpOnly Default true. Flags the cookie to be accessible only by the web server
  * @param {bool} options.signed Default true.  Requires a signature from cookie-parser.  Load that middleware first.
+ * @param {array} options.postMethod Defaults to all post-like methods
  * @api public
  */
 
-const doubleCookie = function (options) {
+const doubleCookie = function (options = {}) {
 
     let {
         always = false,
         secure = true,
         httpOnly = true,
-        signed = true
+        signed = true,
+        postMethod = ['POST', 'PUT', 'PATCH', 'DELETE']
     } = options
 
     const middleware = function (req, res, next) {
 
-        // Utility function
+        // shorthand utility function
         const hash = function (inputStr) {
             return crypto.createHash('sha256').update(inputStr).digest('hex')
         }
 
         // Kill bad requests
-        const postMethods = ['POST', 'PUT', 'PATCH', 'DELETE']
-        if (postMethods.includes(req.method)) {
+        if (postMethod.includes(req.method)) {
             // If there's no cookie or the cookie doesn't match its hash
             if ((!req.signedCookies.dblCookie) || hash(req.signedCookies.dblCookie) !== req.body.postCheck) {
                 return (next(new Error('DoubleCookie CSRF')))
@@ -43,7 +43,7 @@ const doubleCookie = function (options) {
         const reload = function () {
             // If either there's no dblCookie yet or options says to always refresh, reload
             if (!req.signedCookies.dblCookie || always) {
-                var newCookie = random('*',64)
+                var newCookie = crypto.randomBytes(64).toString('hex')
                 res.cookie('dblCookie', newCookie, { signed: signed, httpOnly: httpOnly, secure: secure })
             }
             const dblCookie = newCookie || req.signedCookies.dblCookie
